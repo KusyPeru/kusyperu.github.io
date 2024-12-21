e33ac5e976fe2f22c9cae744e9e882b51c712466
Daniel Angel León Salazar <daniel_angel_leon_salazar@hotmail.com>
2024-12-21 11:18:39 -0500

    🚀 [Code] Add Item Detail for Slug

---
+++ src/data/clothingData.js
@@ -0,0 +1,168 @@
+// imports of men clothing images
+import ImageMenClothing1 from "../assets/clothing/men_clothing/chompa_elegante_cuello_vuelto_hombre_burdeos.webp";
+import ImageMenClothing2 from "../assets/clothing/men_clothing/sueter_elegante_cuello_redondo_hombre_andino.webp";
+import ImageMenClothing3 from "../assets/clothing/men_clothing/polo_cuello_redondo_hombre_gris.webp";
+import ImageMenClothing4 from "../assets/clothing/men_clothing/polo_elegante_casual_hombre_azul_marino.webp";
+import ImageMenClothing5 from "../assets/clothing/men_clothing/polo_elegante_casual_hombre_blanco.webp";
+import ImageMenClothing6 from "../assets/clothing/men_clothing/sueter_elegante_cuello_redondo_hombre_crema.webp";
+
+// imports of woman clothing images
+import ImageWomenClothing1 from "../assets/clothing/women_clothing/enterizo_formal_manga_larga_mujer_rojo.webp";
+import ImageWomenClothing2 from "../assets/clothing/women_clothing/vestido_elegante_largo_mujer_blanco.webp";
+import ImageWomenClothing3 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_semi_transparente.webp";
+import ImageWomenClothing4 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_bordeado_blanco.webp";
+import ImageWomenClothing5 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_rojo.webp";
+import ImageWomenClothing6 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_negro.webp";
+import ImageWomenClothing7 from "../assets/clothing/women_clothing/vestido_largo_fluido_mujer_rojo_1.webp";
+import ImageWomenClothing8 from "../assets/clothing/women_clothing/chompa_elegante_corte_recto_mujer.webp";
+
+// imports of top clothing
+import imgTopClothing1 from "../assets/clothing/women_clothing/vestido_largo_fluido_mujer_rojo_1.webp";
+import imgTopClothing2 from "../assets/clothing/women_clothing/chompa_elegante_corte_recto_mujer.webp";
+import imgTopClothing3 from "../assets/clothing/men_clothing/casaca_elegante_cuero_hombre_negro.webp";
+import imgTopClothing4 from "../assets/clothing/men_clothing/polo_elegante_casual_hombre_azul_marino.webp";
+import imgTopClothing5 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_negro.webp";
+import imgTopClothing6 from "../assets/clothing/men_clothing/polo_cuello_redondo_hombre_gris.webp";
+import imgTopClothing7 from "../assets/clothing/women_clothing/vestido_elegante_midi_mujer_bordeado_blanco.webp";
+
+export const MenClothings = [
+  {
+    name: "Chompa Elegante Cuello Vuelto Burdeos",
+    price: 130.9,
+    image: ImageMenClothing1,
+    slug: "chompa-elegante-cuello-vuelto-burdeos",
+  },
+  {
+    name: "Sueter Elegante Cuello Redondo Andino",
+    price: 139.9,
+    image: ImageMenClothing2,
+    slug: "sueter-elegante-cuello-redondo-andino",
+  },
+  {
+    name: "Polo Cuello Redondo Gris",
+    price: 60.9,
+    image: ImageMenClothing3,
+    slug: "polo-cuello-redondo-gris",
+  },
+  {
+    name: "Polo Elegante Casual Azul Marino",
+    price: 65.9,
+    image: ImageMenClothing4,
+    slug: "polo-elegante-casual-azul-marino",
+  },
+  {
+    name: "Polo Elegante Casual Blanco",
+    price: 65.9,
+    image: ImageMenClothing5,
+    slug: "polo-elegante-casual-blanco",
+  },
+  {
+    name: "Sueter Elegante Cuello Redondo Crema",
+    price: 139.9,
+    image: ImageMenClothing6,
+    slug: "sueter-elegante-cuello-redondo-crema",
+  },
+];
+
+export const WomenClothings = [
+  {
+    name: "Enterizo Formal Manga Larga Rojo",
+    price: 199.9,
+    image: ImageWomenClothing1,
+    slug: "enterizo-formal-manga-larga-rojo",
+  },
+  {
+    name: "Vestido Elegante Largo Blanco",
+    price: 259.9,
+    image: ImageWomenClothing2,
+    slug: "vestido-elegante-largo-blanco",
+  },
+  {
+    name: "Vestido Elegante Midi Semi Transparente",
+    price: 229.9,
+    image: ImageWomenClothing3,
+    slug: "vestido-elegante-midi-semi-transparente",
+  },
+  {
+    name: "Vestido Elegante Midi Bordeado Blanco",
+    price: 219.9,
+    image: ImageWomenClothing4,
+    slug: "vestido-elegante-midi-bordeado-blanco",
+  },
+  {
+    name: "Vestido Elegante Midi Rojo",
+    price: 209.9,
+    image: ImageWomenClothing5,
+    slug: "vestido-elegante-midi-rojo",
+  },
+  {
+    name: "Vestido Elegante Midi Negro",
+    price: 209.9,
+    image: ImageWomenClothing6,
+    slug: "vestido-elegante-midi-negro",
+  },
+  {
+    name: "Vestido Largo Fluido Rojo",
+    price: 189.9,
+    image: ImageWomenClothing7,
+    slug: "vestido-largo-fluido-rojo",
+  },
+  {
+    name: "Chompa Elegante Corte Recto",
+    price: 149.9,
+    image: ImageWomenClothing8,
+    slug: "chompa-elegante-corte-recto",
+  },
+];
+
+export const TopClothing = [
+  {
+    id: 1,
+    name: "Vestido Largo Fluido Rojo",
+    price: 189.9,
+    image: imgTopClothing1,
+    slug: "vestido-largo-fluido-rojo",
+  },
+  {
+    id: 2,
+    name: "Chompa Elegante Corte Recto",
+    price: 149.9,
+    image: imgTopClothing2,
+    slug: "chompa-elegante-corte-recto",
+  },
+  {
+    id: 3,
+    name: "Casaca Elegante de Cuero",
+    price: 299.9,
+    image: imgTopClothing3,
+    slug: "casaca-elegante",
+  },
+  {
+    id: 4,
+    name: "Polo Elegante Casual Azul Marino",
+    price: 65.9,
+    image: imgTopClothing4,
+    slug: "polo-elegante-casual-azul-marino",
+  },
+  {
+    id: 5,
+    name: "Vestido Elegante Midi Negro",
+    price: 209.9,
+    image: imgTopClothing5,
+    slug: "vestido-elegante-midi-negro",
+  },
+  {
+    id: 6,
+    name: "Polo Cuello Redondo Gris",
+    price: 60.9,
+    image: imgTopClothing6,
+    slug: "polo-cuello-redondo-gris",
+  },
+  {
+    id: 7,
+    name: "Vestido Elegante Midi Bordeado Blanco",
+    price: 219.9,
+    image: imgTopClothing7,
+    slug: "vestido-elegante-midi-bordeado-blanco",
+  },
+];
